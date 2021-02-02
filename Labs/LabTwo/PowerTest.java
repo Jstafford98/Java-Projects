0f6f9cd7d3b5977e88bf37e5a393471375cac6ae
Jstafford98 <59448471+Jstafford98@users.noreply.github.com>
2021-02-01 20:16:48 -0800

    This program tests the isPowTwo function in the Power class

---
+++ Labs/LabTwo/PowerTest.java
@@ -0,0 +1,27 @@
+/**
+* Author: Jordan Stafford
+* Problem Statement: This program tests the isPowTwo function in the Power class
+*/
+
+package TestCases;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PowerTest {
+
+	@Test
+	public void test() {
+		
+		Power test = new Power() ;
+		
+		/**
+		 * This function tests all powers of 2 that can fit into an integer value in Java
+		 */
+		for (int i = 1 ; i < 31 ; i++) {//This for loop excludes 2 to the powers of 0 and 31
+			boolean isPow = test.isPowTwo((Math.pow(2, i))) ;
+			assertEquals(true,isPow) ;
+		}
+	}
+
+}

c5b6b34cbc29852f0b28a27d9fb1c2b0cb238ab1
Jstafford98 <59448471+Jstafford98@users.noreply.github.com>
2021-02-27 12:09:18 -0800

    Update Directory.java

--- Labs/LabFour/Directory.java
+++ Labs/LabFour/Directory.java
@@ -1,65 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList ;
 
-// Lab Exercise 4
+class Directory {
+  
+  private static ArrayList<DirectoryEntry> theDirectory ;
+
+  /**
+   * Creates the directory object to store our entries
+   */
+  public Directory() {
+	  theDirectory = new ArrayList<>() ;
+  }
+  /** Add an entry to theDirectory or change an existing entry. 
+  * @param aName the name of the person being added or changed
+  * @param newNumber the new number to be assigned
+  * @return the old number, or if a new entry, null
+  */
+  public static String addOrChangeEntry(String aName, String newNumber){
+
+    String oldNumber ;
+
+    for (DirectoryEntry dE : theDirectory){
+        if(dE.getName().equals(aName)){
+            oldNumber = dE.getNumber() ;
+            dE.setNumber(newNumber) ;
+            return oldNumber ;
+        }
+    }
+
+    theDirectory.add(new DirectoryEntry(aName,newNumber)) ;
+
+    return null ;
+  }
+  /** Remove an entry.
+  * @param aName the name of the person being removed
+  * @return the entry removed, or null if there is no entry for aName
+  */
+  public DirectoryEntry removeEntry(String aName){
+
+    DirectoryEntry oldEntry ;
+
+    for (DirectoryEntry dE : theDirectory){
+        if(dE.getName().equals(aName)){
+            return theDirectory.remove(theDirectory.indexOf(dE)) ;
+        }
+    }
+
+    return null ;
+  }
+  /**
+   * Print all directory entries
+   */
+  public static void printDirectory(){
+      System.out.println("     Current Directory     ") ;
+      System.out.println("---------------------------") ;
+      for (DirectoryEntry dE : theDirectory)
+          System.out.println(dE) ;
+  }
 
 
-public class Directory {
-	private List<DirectoryEntry> theDirectory =
-			new ArrayList<>();
-	
-
-	/** Add an entry to theDirectory or change an existing entry.
-	 * @param aName The name of the person being added or changed
-	 * @param newNumber The new number to be assigned
-	 * @return The old number , or if a new entry, null
-	 */
-	public String addOrChangeEntry(String aName, String newNumber) {
-		DirectoryEntry dE;
-		String oldNumber = null;
-		int index = find(aName);
-
-		if (index != -1)
-			dE = theDirectory.get(index);
-		else
-			dE = null;
-		
-		if(dE==null) 
-			theDirectory.add(new DirectoryEntry(aName,newNumber));
-		else {
-			oldNumber = dE.getNumber();
-			theDirectory.get(index).setNumber(newNumber);
-		}
-		return oldNumber;
-	}
-
-	/** Find an entry in the directory.
-	@param name The name to be found
-	@return The index of the entry with the requested name.
-	If the name is not in the directory, returns -1
-	*/
-		private int find(String name) {
-			int index = 0;
-			for (DirectoryEntry d : theDirectory) {
-				if(d.getName().equals(name))
-					return index;
-				index++;
-			}
-			return -1;
-		}
-	/** Remove an entry.
-	@param aName The name of the person being removed
-	@return The entry removed, or null if there is no entry for aName
-	*/
-
-		public DirectoryEntry removeEntry(String aName) {
-			DirectoryEntry dE;
-			int index = find(aName);
-			if(index != -1) {
-				dE = theDirectory.get(index);
-				theDirectory.remove(theDirectory.remove(index));
-				return dE;
-			}else
-				return null;
-		}
 }
